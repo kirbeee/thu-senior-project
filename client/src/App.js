2a484b93b605e899a6be811c08234c65803422cd
kirbeee <pspsean1234@gmail.com>
2024-08-09 14:12:40 +0800

    add footer

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import {BrowserRouter, Route} from 'react-router-dom';
 
 import Header from './components/Header';
+import Footer from './components/Footer';
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
@@ -19,6 +20,8 @@ class App extends Component {
                         <Route exact path="/" component={Landing}/>
                         <Route exact path="/surveys" component={Dashboard}/>
                         <Route exact path="/surveys/new" component={SurveyNew}/>
+                        <Route/>
+                        <Footer/>
                     </div>
                 </BrowserRouter>
         );
