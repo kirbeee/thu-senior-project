f984a7b3ece612ec3fcbdd644f87ce3c890cbe36
Sean Lin <pspsean1234@gmail.com>
2024-09-12 17:44:23 +0800

    fix header footer missing

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,16 @@ import React from "react";
 import ReactDOM from  "react-dom/client";
 import App from "./App";
 import {NavigationProvider} from "./context/navigation";
+import Header from "./pages/Header";
+import Footer from "./pages/Footer";
 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
 
 root.render(
     <NavigationProvider>
+        <Header/>
         <App/>
+        <Footer />
     </NavigationProvider>
 )
--- client/src/pages/AboutPage.js
+++ client/src/pages/AboutPage.js
@@ -1,8 +1,12 @@
+import Header from "./Header";
+
 function AboutPage() {
     return (
+
         <div>
             <h1 className="text-2xl font-bold">About Page</h1>
             <p className="mt-4">This is the about page</p>
+
         </div>
     )
 }
