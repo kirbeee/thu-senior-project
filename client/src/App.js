d6c19ba43ee578795765d0dc5ecef69735012e6f
Sean Lin <pspsean1234@gmail.com>
2024-09-13 16:32:17 +0800

    add tailwind css style

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,28 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import LandingPage from "./pages/LandingPage";
 import DiscussionPage from "./pages/DiscussionPage";
 import AboutPage from "./pages/AboutPage";
 import Header from "./pages/Header";
 import Footer from "./pages/Footer";
+import Sidebar from "./components/Sidebar";
 
-function App () {
+function App() {
     return (
         <Router>
-            <div>
-                <Header />
-                <Routes>
-                    <Route path="/" element={<LandingPage />} />
-                    <Route path="/about" element={<AboutPage />} />
-                </Routes>
-                <Footer />
+            <div >
+                <Header/>
+                <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
+                    <Sidebar/>
+                    <div className="col-span-5">
+                        <Routes>
+                            <Route path="/" element={<LandingPage/>}/>
+                            <Route path="/discussion" element={<DiscussionPage/>}/>
+                            <Route path="/about" element={<AboutPage/>}/>
+                        </Routes>
+                    </div>
+                </div>
+                <Footer/>
             </div>
         </Router>
     );
