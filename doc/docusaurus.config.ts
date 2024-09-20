a09072a5aa70f2c84a7e032256f9fb31035d13aa
Sean Lin <pspsean1234@gmail.com>
2024-09-20 11:02:56 +0800

    config doc deploy setting

--- doc/docusaurus.config.ts
+++ doc/docusaurus.config.ts
@@ -8,17 +8,18 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://kirbeee.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'kirbeee', // Usually your GitHub org/user name.
+  projectName: 'thu-senior-project', // Usually your repo name.
+  trailingSlash: false,
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
