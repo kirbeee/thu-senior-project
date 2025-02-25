3f8ef39d16aaa304182fa1e34737652926f5e90e
kirbeee <pspsean1234@gmail.com>
2025-02-25 15:56:27 +0800

    fix static css problem

--- backend/thuHelper/settings.py
+++ backend/thuHelper/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 import environ
 import os
 
+from tutorial.settings import BASE_DIR
+
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
@@ -26,7 +28,7 @@ CACHES = {
 BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env(
     # set casting, default value
-    DEBUG=(bool, False)
+    DEBUG=True
 )
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
@@ -202,8 +204,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # 建議的設定方式
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
